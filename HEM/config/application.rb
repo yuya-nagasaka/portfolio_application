1a71de2b661fa98f21c7ca416e6fefc4e2e9d194
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2021-01-30 19:28:38 +0000

    [add]会員登録機能

--- HEM/config/application.rb
+++ HEM/config/application.rb
@@ -5,9 +5,10 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module HEM
   class Application < Rails::Application
+     config.i18n.default_locale = :ja #ロケールを日本語に変更
+     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s] #config/locale以下のファイルを読み込み
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
