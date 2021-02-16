fbed0c2837b27a5942100e2e4b595d7ae71af2ed
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2021-02-16 22:40:26 +0000

    [fix]エラー修正

--- HEM/config/initializers/application_controller_renderer.rb
+++ HEM/config/initializers/application_controller_renderer.rb
@@ -4,4 +4,4 @@
 #   http_host: 'example.org',
 #   https: false
 # )
-ENV['refile_key']
+Refile.secret_key = '1263a2b3e126ff9ab744554b0003de7d54ff1f39a32845f5097feb2f6ce6f7dd25e5537f7cc615e711663ce1b146cfcbb4843dd930402b57e4ec93d2ec8207e0'
