9aaed088689c23972f692f76da7a19db44ab4223
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2021-02-20 05:48:02 +0000

    [fix]変数名の修正

--- HEM/app/controllers/breakfasts_controller.rb
+++ HEM/app/controllers/breakfasts_controller.rb
@@ -2,8 +2,8 @@ class BreakfastsController < ApplicationController
   
   def new
     @breakfast = Breakfast.new
-    @breakfast_recipes = BreakfastRecipe.all
-    @recipes = Recipe.search(params[:search])
+    @all_breakfast_recipes = BreakfastRecipe.all
+    @search_recipes = Recipe.search(params[:search])
     @all_recipes = Recipe.all
   end
  
