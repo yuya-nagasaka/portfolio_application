7054320e453250245120d5f68b34f2333ccf9528
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2021-02-26 17:08:44 +0000

    [add]レシピ一覧ページに投稿ユーザーのリンクを追加

--- HEM/app/controllers/users_controller.rb
+++ HEM/app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update]
 
   def show
-    @recipes = current_user.recipes
+    @recipes = @user.recipes
   end
 
   def edit
