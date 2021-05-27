7a1787b3fa0c00d9d25f237c4b9f1230c1bd79ee
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2021-05-27 12:05:59 +0000

    [fix]昼の食事記録が全ユーザー共通になっていたので、ユーザー別に修正

--- HEM/app/controllers/lunches_controller.rb
+++ HEM/app/controllers/lunches_controller.rb
@@ -3,7 +3,6 @@
 class LunchesController < ApplicationController
   def new
     @lunch = Lunch.new
-    @all_lunch_recipes = LunchRecipe.all
     @search_recipes = Recipe.search(params[:search])
     @search_recipes = @search_recipes.page(params[:page]).per(5)
     @all_recipes = Recipe.all
